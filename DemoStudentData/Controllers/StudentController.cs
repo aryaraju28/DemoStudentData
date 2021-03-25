6b6db21287ce8fcbf015692cc08107411153be89
aryaraju28 <arya28199@gmail.com>
2021-03-25 11:58:24 +0530

    Add project files.

---
+++ DemoStudentData/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoStudentData.Models;
+using System.Data;
+
+using System.Data.SqlClient;
+
+
+namespace DemoStudentData.Controllers
+{
+    public class StudentController : Controller
+    {
+        String connectionString = "Server=RAED_COMPUTER\\SQLEXPRESS;Database=SchoolManagementSystem;Trusted_Connection=True;";
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<User> users = new List<User>();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SaveStudent", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var user  = new User();
+                user.Id = Convert.ToInt32(reader["Id"]);
+                user.Name = reader["Name"].ToString();
+                user.Malayalam = Convert.ToInt32(reader["Malayalam"]);
+                user.English = Convert.ToInt32(reader["English"]);
+                user.Maths = Convert.ToInt32(reader["Maths"]);
+                users.Add(user);
+            }
+
+            return View(users);
+        }
+    }
+}
---
+++ DemoStudentData/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoStudentData.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Malayalam { get; set; }
+        public int English { get; set; }
+        public int Maths { get; set; }
+    }
+}
