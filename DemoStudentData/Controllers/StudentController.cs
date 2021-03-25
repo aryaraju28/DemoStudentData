ac6028c49cb9228055b3e05deb41497603b8ec91
aryaraju28 <arya28199@gmail.com>
2021-03-25 20:01:24 +0530

    Last commit

--- DemoStudentData/Controllers/StudentController.cs
+++ DemoStudentData/Controllers/StudentController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using DemoStudentData.Models;
 using System.Data;
-
+using System.Configuration;
 using System.Data.SqlClient;
 
 
@@ -13,29 +13,36 @@ namespace DemoStudentData.Controllers
 {
     public class StudentController : Controller
     {
-        String connectionString = "Server=RAED_COMPUTER\\SQLEXPRESS;Database=SchoolManagementSystem;Trusted_Connection=True;";
+        //String connectionString = "Server=RAED_COMPUTER\\SQLEXPRESS;Database=SchoolManagementSystem;Trusted_Connection=True;";
         // GET: Student
         public ActionResult Index()
         {
             List<User> users = new List<User>();
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-
-            SqlCommand command = new SqlCommand("SaveStudent", connection);
-            command.CommandType = CommandType.StoredProcedure;
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+            string connection = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
             {
-                var user  = new User();
-                user.Id = Convert.ToInt32(reader["Id"]);
-                user.Name = reader["Name"].ToString();
-                user.Malayalam = Convert.ToInt32(reader["Malayalam"]);
-                user.English = Convert.ToInt32(reader["English"]);
-                user.Maths = Convert.ToInt32(reader["Maths"]);
-                users.Add(user);
-            }
 
-            return View(users);
+                //SqlConnection connection = new SqlConnection(connectionString);
+
+
+                SqlCommand command = new SqlCommand("SaveStudent", con);
+                command.CommandType =CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var user = new User();
+                    user.Id = Convert.ToInt32(reader["Id"]);
+                    user.Name = reader["Name"].ToString();
+                    user.Malayalam = Convert.ToInt32(reader["Malayalam"]);
+                    user.English = Convert.ToInt32(reader["English"]);
+                    user.Maths = Convert.ToInt32(reader["Maths"]);
+                    users.Add(user);
+                }
+                
+
+                return View(users);
+            }
         }
     }
 }
